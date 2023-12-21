e2db697e44bdc783b26db58ce40a92b20ca490d8
Dan Willett <danwillett@ucsb.edu>
2023-12-21 13:51:57 -0800

    added ped/bike filter to map

--- dashboard/src/components/apps/VolumeSliderMap.js
+++ dashboard/src/components/apps/VolumeSliderMap.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import { loadModules } from "esri-loader";
-// import MapForm from "./MapForm";
+import VolumeSliderOptions from "./VolumeSliderOptions";
 
 export default function VolumeSliderMap() {
+
+  const [filterOptions, setFilterOptions] = useState({
+    bikes: true,
+    peds: false,
+  });
+  const [showForm, setShowForm] = useState(true)
+  const handleApplyOptions = (options) => {
+    // Update the filter options when the user applies changes
+    setFilterOptions(options);
+  };
 
   const mapRef = useRef();
   const infoRef = useRef();
@@ -12,6 +22,7 @@ export default function VolumeSliderMap() {
   const legend = useRef(null);
 
   useEffect(() => {
+
     const initMap = async () => {
       const [Map, FeatureLayer, LayerView, MapView, esriConfig, FeatureTable, Query, GraphicsLayer, Graphic, Slider, Legend, Expand, UniqueValueRenderer] =
         await loadModules([
@@ -32,16 +43,32 @@ export default function VolumeSliderMap() {
         ]);
       console.log(esriConfig);
 
-      const bikeVolumes = new FeatureLayer({
+      const walkingUrl = "https://services1.arcgis.com/4TXrdeWh0RyCqPgB/arcgis/rest/services/ATP_Volumes_SB/FeatureServer/0"
+      const bikingUrl = "https://services1.arcgis.com/4TXrdeWh0RyCqPgB/arcgis/rest/services/ATP_Volumes_SB/FeatureServer/2"
+      let url
+      let name
+   
+      if (filterOptions.bikes) {
+        url = bikingUrl
+        name = "bike"
+        console.log(name)
+      } else if (filterOptions.peds) {
+        url = walkingUrl
+        name = "ped"
+        console.log(name)
+      }
+      
+      const volumes = new FeatureLayer({
+        url: url
+      })
+
+
+      const sites = new FeatureLayer({
         url: "https://services1.arcgis.com/4TXrdeWh0RyCqPgB/arcgis/rest/services/ATP_Volumes_SB/FeatureServer/1"
       })
 
-      const sites = new FeatureLayer({
-        url: "https://services1.arcgis.com/4TXrdeWh0RyCqPgB/arcgis/rest/services/ATP_Volumes_SB/FeatureServer/0"
-      })
-
       const hourFields = ["h_00", "h_01", "h_02", "h_03", "h_04", "h_05", "h_06", "h_07", "h_08", "h_09", "h_10", "h_11", "h_12", "h_13", "h_14", "h_15", "h_16", "h_17", "h_18", "h_19", "h_20", "h_21", "h_22", "h_23"]
-      const bikeResultsLayer = new GraphicsLayer()
+      const resultsLayer = new GraphicsLayer()
 
       const sitesQuery = new Query();
       sitesQuery.where = "";  // query all
@@ -57,34 +84,39 @@ export default function VolumeSliderMap() {
           siteObj[siteLocation]['attributes'] = feature.attributes
           siteObj[siteLocation]['geometry'] = feature.geometry
         })
-        console.log(siteObj)
+
 
         let statsObj = hourFields.map((field) => (
           {
-            onStatisticField: `bike_volumes_${field}`,
+            onStatisticField: `${name}_volumes_${field}`,
             outStatisticFieldName: "avg" + field,
             statisticType: "avg",
           }
         ))
 
+
         const statsQuery = new Query({
           outStatistics: statsObj,
-          groupByFieldsForStatistics: ["bike_volumes_location"],
+          groupByFieldsForStatistics: [`${name}_volumes_location`],
           returnGeometry: false,
         });
 
-        bikeVolumes.queryFeatures(statsQuery).then((result) => {
+        volumes.queryFeatures(statsQuery).then((result) => {
+          
           const volumeFeatures = result.features
 
           volumeFeatures.forEach(feature => {
-            let volumeLocation = feature.attributes['bike_volumes_location']
 
-            let graphicFeature = siteObj[volumeLocation]
+            let volumeLocation = feature.attributes[`${name}_volumes_location`]
+            if (volumeLocation) {
+              let graphicFeature = siteObj[volumeLocation]
 
             hourFields.forEach(hour => {
               let hourKey = `avg${hour}`
               graphicFeature['attributes'][hourKey] = feature.attributes[hourKey]
             })
+ 
+            graphicFeature['attributes']['type'] = name
 
 
             if (graphicFeature['geometry']) {
@@ -93,8 +125,10 @@ export default function VolumeSliderMap() {
                 attributes: graphicFeature.attributes,
                 // symbol: symbol
               })
-              bikeResultsLayer.add(graphic)
+              resultsLayer.add(graphic)
             }
+            }
+            
 
           })
       
@@ -110,24 +144,8 @@ export default function VolumeSliderMap() {
       // Create a map
       const map = new Map({
         basemap: "streets-vector",
-        layers: [bikeResultsLayer]
+        layers: [resultsLayer]
       });
-
-      console.log(map)
-
-      // Create a symbol function based on color
-      function createSymbol(color) {
-        return {
-          type: "simple-marker",
-          style: "circle",
-          color: color,
-          size: 10,
-          outline: {
-            color: "white",
-            width: 1,
-          },
-        };
-      }
 
       // Create a view
       const view = new MapView({
@@ -148,10 +166,8 @@ export default function VolumeSliderMap() {
         "top-right"
       );
       
-      console.log(bikeResultsLayer)
-      view.whenLayerView(bikeResultsLayer).then((layerView) => {
-        
-        console.log(layerView)
+      view.whenLayerView(resultsLayer).then((layerView) => {
+
         if (!legend.current) {
           legend.current = new Legend({
             view: view,
@@ -164,14 +180,12 @@ export default function VolumeSliderMap() {
             // ],
           });
         }
-        console.log(legend.current)
-        
         
         if (!slider.current) {
           slider.current = new Slider({
             min: 0,
             max: 23,
-            values: [0],
+            values: [12],
             container: sliderRef.current,
             visibleElements: {
               rangeLabels: true
@@ -194,14 +208,13 @@ export default function VolumeSliderMap() {
         }
   
         slider.current.on(["thumb-change", "thumb-drag"], (event) => {
-  
-          console.log(view)
+
           let fieldToShow = `avg${hourFields[event.value]}`
   
-          let pointsAmount = bikeResultsLayer.graphics.items.length
+          let pointsAmount = resultsLayer.graphics.items.length
           let updatedGraphics = []
           for (let i = 0; i < pointsAmount; i++) {
-            let countAmount = bikeResultsLayer.graphics.items[i].attributes[fieldToShow]
+            let countAmount = resultsLayer.graphics.items[i].attributes[fieldToShow]
             let symbolSize = symbolRange(countAmount)
             let symbol = {
                 type: "simple-marker",
@@ -214,11 +227,11 @@ export default function VolumeSliderMap() {
             
               }
               
-              bikeResultsLayer.graphics.items[i].symbol = symbol
+              resultsLayer.graphics.items[i].symbol = symbol
               let updatedGraphic = new Graphic({
-                geometry: bikeResultsLayer.graphics.items[i].geometry,
+                geometry: resultsLayer.graphics.items[i].geometry,
                 symbol: symbol,
-                attributes: bikeResultsLayer.graphics.items[i].attributes,
+                attributes: resultsLayer.graphics.items[i].attributes,
               });
           
               updatedGraphics.push(updatedGraphic);
@@ -227,10 +240,10 @@ export default function VolumeSliderMap() {
           sliderValue.innerText = event.value;
   
           // Remove all graphics from the layer
-          bikeResultsLayer.graphics.removeAll();
+          resultsLayer.graphics.removeAll();
 
           // Add the updated graphics to the layer
-          bikeResultsLayer.graphics.addMany(updatedGraphics);  
+          resultsLayer.graphics.addMany(updatedGraphics);  
   
         });
   
@@ -240,22 +253,31 @@ export default function VolumeSliderMap() {
     };
 
     initMap();
-  }, []);
+
+    
+  }, [filterOptions]);
 
 
+  const handleCloseForm = () => {
+    setShowForm(false);
+    console.log("closing form")
+  };
 
   return (
     <div>
-      <div ref={mapRef} style={{ height: "70vh", width: "70vw" }}></div>
-      <div ref={infoRef}>
-        <div id="description">
-          Show power plants with at least <span id="sliderValue">0</span> megawatts of capacity
+
+        <div ref={mapRef} style={{ height: "70vh", width: "70vw" }}></div>
+        <div ref={infoRef}>
+          <div id="description">
+            Show power plants with at least <span id="sliderValue">0</span> megawatts of capacity
+          </div>
+          <div id="sliderContainer">
+            <div ref={sliderRef}></div>
+          </div>
+          <div ref={legendRef}></div>
         </div>
-        <div id="sliderContainer">
-          <div ref={sliderRef}></div>
-        </div>
-        <div ref={legendRef}></div>
-      </div>
+    
+      {showForm && <VolumeSliderOptions showForm={showForm} onApplyOptions={handleApplyOptions} onClose={handleCloseForm} />}
     </div>
   );
 }
--- dashboard/src/components/apps/VolumeSliderOptions.js
+++ dashboard/src/components/apps/VolumeSliderOptions.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { loadModules } from "esri-loader";
-// import MapForm from "./MapForm";
+
 
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button';
@@ -10,58 +10,65 @@ export default function VolumeSliderOptions(inputs) {
     console.log(inputs)
     const showForm = inputs.showForm
     const handleClose = inputs.onClose
-    const {latitude, longitude} = inputs.coordinates
+    const onApplyOptions = inputs.onApplyOptions
 
+    const [selectedOptions, setSelectedOptions] = useState({
+        bikes: false,
+        peds: false,
+      });
+    
+    const handleCheckboxChange = (name) => {
+        // Update the selected options when a checkbox is changed
+        setSelectedOptions((prevOptions) => ({
+            ...prevOptions,
+            [name]: !prevOptions[name],
+        }));
+    };
+    
+    const handleApplyOptions = () => {
+        // Call the parent component's callback with the selected options
+        onApplyOptions(selectedOptions);
+        handleClose()
+    };
 
-    console.log(latitude)
-    // submit form which posts values to database
-    // update form values when typing so they can view their selection... need useState() for this
-
-    // when submitted successfully, return with a response that says "yay! sucessfully returned!!"
-
-    // then we go crazy on it all
     return (
-        <div
-            className= "modal show d-flex align-items-center"
-            // style= {{height: "70vh", width: '50vw'}}
+        <div className="modal show d-flex align-items-center"
+        // style= {{height: "70vh", width: '50vw'}}
         >
             <Modal show={showForm} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add Your Favorite Place!</Modal.Title>
+                    <Modal.Title>Select Options</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-
-                    <h2>Hey</h2>
-                    <p>{latitude}</p>
-                    <p>{longitude}</p>
-
                     <Form>
-                        <Form.Group className="mb-3" controlId="formSomething">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
                         
+                        <Form.Check
+                            inline
+                            label="Bike Counts"
+                            name="bikes"
+                            type="checkbox"    
+                            onClick={() => handleCheckboxChange("bikes")}
+                            id={`bike-check`}
+                        />
+                        <Form.Check
+                            inline
+                            label="Pedestrian Counts"
+                            name="peds"
+                            type="checkbox"
+                            onClick={() => handleCheckboxChange("peds")}
+                            id={`ped-check`}
+                        />
+                       
                     </Form>
 
                 </Modal.Body>
 
                 <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>Discard</Button>
-                <Button variant="primary" type="submit" onClick={handleClose}>Submit</Button>
+                    <Button variant="secondary" onClick={handleClose}>Discard</Button>
+                    <Button variant="primary" type="submit" onClick={handleApplyOptions}>Submit</Button>
                 </Modal.Footer>
             </Modal>
-            
+
 
         </div>
     )
