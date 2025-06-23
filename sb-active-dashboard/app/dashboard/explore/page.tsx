1df59ce4e48cca07f6e97ba6a53cb3a78dd42c68
William Overbye-Thompson <49210425+wjthompso@users.noreply.github.com>
2025-06-23 15:48:03 -0700

    Moving files closer to root

--- sb-active-dashboard/app/dashboard/explore/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-const ExploreMapNoSSR = dynamic(
-  () => import("../../ui/explore-app/ExploreMap"),
-  { ssr: false }
-);
-
-export default function Explore() {
-  return <ExploreMapNoSSR />;
-}
